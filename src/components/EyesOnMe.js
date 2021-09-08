bb7dd41ce3fab2c52505eb11987a5f7a70a163b4
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-09-08 11:04:56 -0400

    stopped for help

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
-// Code EyesOnMe Component Here
+
+function EyesOnMe() {
+    function areYouWatching(event) {
+        //if event is blur log "Hey! Eyes on me!"
+        //if event is focus log "Good!"
+    }
+    return (
+       <button onClick={}>Eyes in me!</button> 
+    );
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
-// Code Keypad Component Here
+function Keypad() {
+    function handleChange(event) {
+        console.log("Entering password...");
+    }
+  return (
+    <input type="password"  onChange={handleChange} placeholder="Enter password"/>
+  );
+}
+export default Keypad;
